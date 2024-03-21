9cb349c8643c8d0576d0a9fe1042c568c4747d58
vboxuser <vboxuser@Ubuntu22.04.myguest.virtualbox.org>
2024-03-21 14:26:24 +0800

    Add state in patient_master

--- custom_addons/hospital_management/models/patient_master.py
+++ custom_addons/hospital_management/models/patient_master.py
@@ -8,8 +8,8 @@ class patient_master(models.Model):
     date = fields.Datetime(string="Date", default=fields.Datetime.now())
     name = fields.Char(string="Name")
     gender = fields.Selection([
-                ('male', 'Male'),
-                ('female', 'Female')], string="Gender")
+        ('male', 'Male'),
+        ('female', 'Female')], string="Gender")
     age = fields.Float(string="Age")
     mobile = fields.Float(string="Mobile")
     symptoms = fields.Text(string="Symptoms", required=True)
@@ -17,3 +17,26 @@ class patient_master(models.Model):
     cure_for_disease = fields.Text(string="Cure for Disease", required=True)
     medicines = fields.Text(string="Medicines", required=True)
     cancellation_remark = fields.Text(string="Cancellation Remark")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_progress', 'In Progress'),
+        ('sent_email', 'Sent Email'),
+        ('approved', 'Approved'),
+        ('done', 'Done'),
+        ('cancel', 'Cancel')
+    ], default="draft", string="State", required=True)
+
+    def action_in_progress(self):
+        self.state = 'in_progress'
+
+    def action_sent_email(self):
+        self.state = 'sent_email'
+
+    def action_approved(self):
+        self.state = 'approved'
+
+    def action_done(self):
+        self.state = 'done'
+
+    def action_cancel(self):
+        self.state = 'draft'
