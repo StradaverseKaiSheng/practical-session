989e68d0e45f4d31544d92f0b56777f322cfd4ad
vboxuser <vboxuser@Ubuntu22.04.myguest.virtualbox.org>
2024-03-22 17:50:47 +0800

    Update on models

--- custom_addons/insurance_management/models/agent_master.py
+++ custom_addons/insurance_management/models/agent_master.py
@@ -3,29 +3,44 @@
 from odoo import models, fields, api
 
 
-class PolicyMaster(models.Model):
-    _name = 'policy.master'
-    _description = 'Policy Master'
+class AgentMaster(models.Model):
+    _name = 'agent.master'
+    _description = 'Agent Master'
 
     code = fields.Float(string="Code")
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", required=True)
     contact_detail = fields.Float(string="Contact Detail")
     email_id = fields.Char(string="Email ID")
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')], string="Gender")
-    related_user_ids = fields.Many2one('res_users', string="Related User")
+    related_user_ids = fields.Many2one('res.users', string="Related User")
     commission_type = fields.Selection([
         ('fixed', 'Fixed'),
         ('based_on_percentage', 'Based on Percentage')], string="Commission Type")
     amount = fields.Float(string="Amount")
     percentage = fields.Float(string="Percentage")
     total_commission = fields.Float(string="Total Commission")
+    insurance_id = fields.One2many('insurance', string='Insurance')
+    name = fields.Text(related='insurance.name', string='Name', readonly=True)
+    customer_ids = fields.Many2one(related='insurance.customer_ids', string='Customer', readonly=True)
+    policy_ids = fields.Many2one(related='insurance.policy_ids', string='Policy', readonly=True)
+    amount = fields.float(related='insurance.amount', string='Amount', readonly=True)
+    total_commission_amount = fields.float(related='insurance.total_commission_amount', string='Total Commission Amount', readonly=True)
+    state = fields.Selection(related='insurance.state', string='State', readonly=True)
 
-    # insurance_ids = fields.One2many('', string='Insurance')
-    # name = fields.Text(related='patient_name.symptoms', string='Name', readonly=True)
-    # customer_id = fields.Many2one(related='patient_name.disease', string='Customer', readonly=True)
-    # policy_id = fields.Many2one(related='patient_name.cure_for_disease', string='Policy', readonly=True)
-    # amount = fields.float(related='patient_name.medicines', string='Amount', readonly=True)
-    # total_commission_amount = fields.float(related='patient_name.medicines', string='Total Commission Amount', readonly=True)
-    # state = fields.Selection(related='patient_name.medicines', string='State', readonly=True)
+    # @api.depends('premium_paying_period', 'payment_mode', 'premium_amount')
+    # def _compute_total_commission(self):
+    #     for record in self:
+    #         if record.payment_type == 'installment':
+    #             if record.payment_mode =='yearly':
+    #                 record.total_policy_amount = record.premium_paying_period * record.premium_amount
+    #             elif record.payment_mode =='half_yearly':
+    #                 record.total_policy_amount = record.premium_paying_period * record.premium_amount * 2
+    #             elif record.payment_mode =='quarterly':
+    #                 record.total_policy_amount = record.premium_paying_period * record.premium_amount * 3
+    #             else:
+    #                 record.total_policy_amount = record.premium_paying_period * record.premium_amount * 12
+    #         else:
+    #             # temp
+    #              record.total_policy_amount = record.premium_paying_period * record.premium_amount
--- custom_addons/insurance_management/models/insurance.py
+++ custom_addons/insurance_management/models/insurance.py
@@ -8,20 +8,24 @@ class Insurance(models.Model):
     _description = 'Insurance'
 
     name = fields.Char(string="Name")
-    customer_ids = fields.Many2one(string="Contact Detail")
-    policy_ids = fields.Many2one(string="Email ID")
-    agent_ids = fields.Many2one(string="Gender")
-    start_date = fields.Date(string="Related User")
-    maturity_date = fields.Date(string="Commission Type")
-    premium_paying_perid = fields.Float(string="Amount")
-    payment_mode = fields.Float(string="Percentage")
-    total_policy_amount = fields.Float(string="Total Commission")
-    total_commission_amount = fields.Float(string="Total Commission")
+    customer_ids = fields.Many2one('res.partner', string="Customer")
+    policy_ids = fields.Many2one('policy.master', string="Policy")
+    agent_ids = fields.Many2one('agent.master', string="Agent")
+    start_date = fields.Date(string="Start Date", default=fields.Date.now())
+    maturity_date = fields.Date(string="Maturity Date") # computed later
+    premium_paying_perid = fields.Integer(related='policy_ids.premium_paying_perid', string="Premium Paying Period (In year)")
+    payment_type = fields.Selection(related='policy_ids.payment_type', string="Payment Type")
+    payment_mode = fields.Selection(related='policy_ids.payment_mode', string="Payment Mode")
+    premium_amount = fields.Float(related='policy_ids.total_policy_amount', string="Premium Amount")
+    total_policy_amount = fields.Float(related='policy_ids.total_policy_amount', string="Total Policy Amount")
+    total_commission_amount = fields.Float(string="Total Commission Amount") # computed later
+
     no_of_premium = fields.One2many()
     number = fields.One2many()
     due_date = fields.One2many()
     premium_amount = fields.One2many()
     commission_amount = fields.One2many()
+    # state = fields.Selection(related='patient_name.medicines', string='State', readonly=True)
 
     # insurance_ids = fields.One2many('', string='Insurance')
     # name = fields.Text(related='patient_name.symptoms', string='Name', readonly=True)
