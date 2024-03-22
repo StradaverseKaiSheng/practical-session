87bd9d9978adc719cb7d04f2913bce6a46cab86e
vboxuser <vboxuser@Ubuntu22.04.myguest.virtualbox.org>
2024-03-22 15:21:10 +0800

    Update policy_master model's name

--- custom_addons/insurance_management/models/policy_master.py
+++ custom_addons/insurance_management/models/policy_master.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 
 
 class PolicyMaster(models.Model):
-    _name = 'insurance_management.policy_master'
-    _description = 'insurance_management.policy_master'
+    _name = 'policy.master'
+    _description = 'Policy Master'
 
     code = fields.Float(string="Code")
     name = fields.Char(string="Name")
@@ -25,3 +25,8 @@ class PolicyMaster(models.Model):
     premium_amount = fields.Float(string="Premium Amount")
     total_policy_amount = fields.Float(string="Total Policy Amount")
     notes = fields.Html(string="Notes")
+
+    # @api.depends('premium_paying_period', 'payment_mode', 'premium_amount')
+    # def _compute_total_policy_amount(self):
+    #     for record in self:
+    #         record.total_policy_amount = record.field1 + record.field2
